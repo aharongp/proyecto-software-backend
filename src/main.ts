5f1d7e25c739ac79e51548cda46b0a1286a0a6ac
sdfl2006 <santidefreitas@gmail.com>
2025-05-27 17:11:59 -0400

    activar cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors(); // Habilita CORS
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
 
